6d636a96439a01010523b9c3512ecf1f5b61e338
Eells, Andrew : Group Centre <andrew.eells@barclays.co.uk>
2015-05-01 14:01:07 +0100

    Adding request header.

--- src/main/java/org/force66/correlate/RequestCorrelationFilter.java
+++ src/main/java/org/force66/correlate/RequestCorrelationFilter.java
@@ -2,9 +2,9 @@
  * This software is licensed under the Apache License, Version 2.0
  * (the "License") agreement; you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,8 +13,10 @@
  */
 package org.force66.correlate;
 
-import java.io.IOException;
-import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -23,84 +25,99 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
- * Checks for correlation id in the header.  If it doesn't exist, 
+ * Checks for correlation id in the header.  If it doesn't exist,
  * establishes a new correlation id.
- * 
+ * <p/>
  * <p>Initialization parameters are as follows:</p>
- * <li>correlation.id.header - header name containing the correlation id.  
+ * <li>correlation.id.header - header name containing the correlation id.
  * Default is requestCorrelationId.</li>
- * @author D. Ashmore
  *
+ * @author D. Ashmore
  */
-public class RequestCorrelationFilter implements Filter {
-	
-	public static final String DEFAULT_CORRELATION_ID_HEADER_NAME="requestCorrelationId";
-	public static final String DEFAULT_LOGGER_MDC_NAME="requestId";
-	
-	public static final String INIT_PARM_CORRELATION_ID_HEADER="correlation.id.header";
-	public static final String INIT_PARM_LOGGER_NDC_NAME="logger.mdc.name";
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(RequestCorrelationFilter.class);
-	
-	private String correlationHeaderName;
-	private String loggerMdcName;
+public class RequestCorrelationFilter implements Filter
+{
+    public static final String DEFAULT_CORRELATION_ID_HEADER_NAME = "requestCorrelationId";
 
-	public void init(FilterConfig filterConfig) throws ServletException {
-		String headerName = filterConfig.getInitParameter(INIT_PARM_CORRELATION_ID_HEADER);
-		if (StringUtils.isEmpty(headerName)) {
-			correlationHeaderName=DEFAULT_CORRELATION_ID_HEADER_NAME;
-		}
-		else {
-			correlationHeaderName=headerName.trim();
-		}
-		
-		String mdcName = filterConfig.getInitParameter(INIT_PARM_LOGGER_NDC_NAME);
-		if (StringUtils.isEmpty(mdcName)) {
-			loggerMdcName=DEFAULT_LOGGER_MDC_NAME;
-		}
-		else {
-			loggerMdcName=mdcName.trim();
-		}
-	}
+    public static final String DEFAULT_LOGGER_MDC_NAME = "requestId";
 
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		String correlationId = httpServletRequest.getHeader(correlationHeaderName);
-		if (StringUtils.isEmpty(correlationId)) {
-			correlationId = UUID.randomUUID().toString();
-		}
-				
-		RequestCorrelationContext.getCurrent().setCorrelationId(correlationId);
-		MDC.put(loggerMdcName, correlationId);
-		LOGGER.debug("Correlation id={} request={}", correlationId, httpServletRequest.getPathInfo());
-		
-		try {chain.doFilter(httpServletRequest, response);}
-		finally {
-			MDC.remove(loggerMdcName);
-			RequestCorrelationContext.clearCurrent();
-		}
+    public static final String INIT_PARM_CORRELATION_ID_HEADER = "correlation.id.header";
 
-	}
+    public static final String INIT_PARM_LOGGER_NDC_NAME = "logger.mdc.name";
 
-	public void destroy() {
-		// NoOp
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestCorrelationFilter.class);
 
-	}
+    private String correlationHeaderName;
 
-	protected String getCorrelationHeaderName() {
-		return correlationHeaderName;
-	}
+    private String loggerMdcName;
 
-	protected String getLoggerMdcName() {
-		return loggerMdcName;
-	}
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+        String headerName = filterConfig.getInitParameter(INIT_PARM_CORRELATION_ID_HEADER);
+        if (StringUtils.isEmpty(headerName))
+        {
+            correlationHeaderName = DEFAULT_CORRELATION_ID_HEADER_NAME;
+        }
+        else
+        {
+            correlationHeaderName = headerName.trim();
+        }
 
+        String mdcName = filterConfig.getInitParameter(INIT_PARM_LOGGER_NDC_NAME);
+        if (StringUtils.isEmpty(mdcName))
+        {
+            loggerMdcName = DEFAULT_LOGGER_MDC_NAME;
+        }
+        else
+        {
+            loggerMdcName = mdcName.trim();
+        }
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String correlationId = httpServletRequest.getHeader(correlationHeaderName);
+        if (StringUtils.isEmpty(correlationId))
+        {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        RequestCorrelationContext.getCurrent().setCorrelationId(correlationId);
+        MDC.put(loggerMdcName, correlationId);
+        LOGGER.debug("Correlation id={} request={}", correlationId, httpServletRequest.getPathInfo());
+
+        try
+        {
+            final MutableHttpServletRequest requestWrapper = new MutableHttpServletRequest(httpServletRequest);
+            requestWrapper.putHeader(correlationHeaderName, correlationId);
+
+            chain.doFilter(requestWrapper, response);
+        }
+        finally
+        {
+            MDC.remove(loggerMdcName);
+            RequestCorrelationContext.clearCurrent();
+        }
+
+    }
+
+    public void destroy()
+    {
+        // NoOp
+
+    }
+
+    protected String getCorrelationHeaderName()
+    {
+        return correlationHeaderName;
+    }
+
+    protected String getLoggerMdcName()
+    {
+        return loggerMdcName;
+    }
 }
